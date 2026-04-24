e55d932811b3dc45de0877c19229a7a965c35af0
baptiste <collebaptiste@gmail.com>
2026-04-24 15:03:39 +0100

    Infra: fix Render observability service wiring

--- docker/images/vmalert/start-vmalert.sh
+++ docker/images/vmalert/start-vmalert.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -eu
 
-: "${CMDCLAW_VICTORIA_METRICS_HOSTPORT:?cmdclaw victoria metrics hostport is required}"
-: "${CMDCLAW_ALERTMANAGER_HOSTPORT:?cmdclaw alertmanager hostport is required}"
+: "${CMDCLAW_VICTORIA_METRICS_HOST:?cmdclaw victoria metrics host is required}"
+: "${CMDCLAW_ALERTMANAGER_HOST:?cmdclaw alertmanager host is required}"
 
 exec vmalert \
   -rule=/etc/vmalert/rules/*.yml \
-  -datasource.url="http://${CMDCLAW_VICTORIA_METRICS_HOSTPORT}" \
-  -remoteWrite.url="http://${CMDCLAW_VICTORIA_METRICS_HOSTPORT}" \
-  -remoteRead.url="http://${CMDCLAW_VICTORIA_METRICS_HOSTPORT}" \
-  -notifier.url="http://${CMDCLAW_ALERTMANAGER_HOSTPORT}" \
+  -datasource.url="http://${CMDCLAW_VICTORIA_METRICS_HOST}:8428" \
+  -remoteWrite.url="http://${CMDCLAW_VICTORIA_METRICS_HOST}:8428" \
+  -remoteRead.url="http://${CMDCLAW_VICTORIA_METRICS_HOST}:8428" \
+  -notifier.url="http://${CMDCLAW_ALERTMANAGER_HOST}:9093" \
   -evaluationInterval=30s \
   -rule.evalDelay=30s \
   -httpListenAddr=:8880
