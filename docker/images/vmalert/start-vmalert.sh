88ff370c0295f6a6970da0cda98311be61e975ad
baptiste <collebaptiste@gmail.com>
2026-04-24 15:06:52 +0100

    Infra: fix vmalert entrypoint on Render

--- docker/images/vmalert/start-vmalert.sh
+++ docker/images/vmalert/start-vmalert.sh
@@ -4,7 +4,7 @@ set -eu
 : "${CMDCLAW_VICTORIA_METRICS_HOST:?cmdclaw victoria metrics host is required}"
 : "${CMDCLAW_ALERTMANAGER_HOST:?cmdclaw alertmanager host is required}"
 
-exec vmalert \
+exec /vmalert-prod \
   -rule=/etc/vmalert/rules/*.yml \
   -datasource.url="http://${CMDCLAW_VICTORIA_METRICS_HOST}:8428" \
   -remoteWrite.url="http://${CMDCLAW_VICTORIA_METRICS_HOST}:8428" \
