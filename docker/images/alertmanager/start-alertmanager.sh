6e9a21d09aef2be09514f9b1d35e53bdf759ccd2
baptiste <collebaptiste@gmail.com>
2026-04-25 06:29:22 +0100

    Infra: expose Alertmanager on Render staging

--- docker/images/alertmanager/start-alertmanager.sh
+++ docker/images/alertmanager/start-alertmanager.sh
@@ -7,4 +7,4 @@ printf '%s' "${SLACK_BOT_TOKEN:-}" > /etc/alertmanager/secrets/slack_bot_token
 exec /bin/alertmanager \
   --config.file=/etc/alertmanager/alertmanager.yml \
   --storage.path=/alertmanager \
-  --web.listen-address=:9093
+  --web.listen-address=":${PORT:-9093}"
